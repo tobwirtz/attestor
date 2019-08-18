455840325dbee4eb8a32990fa4577da5cf5809e0
Tobias Wirtz <tobi.wirtz@hotmail.de>
2019-08-18 18:26:49 +0200

    added new package for library support

---
+++ src/main/java/de/rwth/i2/attestor/semantics/jimpleSemantics/jimple/statements/JavaLibrarySupport/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This Package contains the Statements and methods that are necessary for supporting Java Libraries
+ *
+ * @author Tobias
+ */
+
+package de.rwth.i2.attestor.semantics.jimpleSemantics.jimple.statements.JavaLibrarySupport;
