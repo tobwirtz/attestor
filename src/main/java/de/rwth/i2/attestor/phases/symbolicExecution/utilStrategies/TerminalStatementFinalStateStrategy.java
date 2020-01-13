5b809cbedc617b8a24a4e929c7857c74f2bd9de8
Tobias Wirtz <tobi.wirtz@hotmail.de>
2020-01-13 15:41:53 +0100

    ExceptionProgramStates are now treated as final states. If try/catch blocks are used, this needs fixing!

--- src/main/java/de/rwth/i2/attestor/phases/symbolicExecution/utilStrategies/TerminalStatementFinalStateStrategy.java
+++ src/main/java/de/rwth/i2/attestor/phases/symbolicExecution/utilStrategies/TerminalStatementFinalStateStrategy.java
@@ -1,5 +1,6 @@
 package de.rwth.i2.attestor.phases.symbolicExecution.utilStrategies;
 
+import de.rwth.i2.attestor.programState.defaultState.ExceptionProgramState;
 import de.rwth.i2.attestor.semantics.TerminalStatement;
 import de.rwth.i2.attestor.stateSpaceGeneration.FinalStateStrategy;
 import de.rwth.i2.attestor.stateSpaceGeneration.ProgramState;
@@ -13,7 +14,8 @@ public class TerminalStatementFinalStateStrategy implements FinalStateStrategy {
     public boolean isFinalState(ProgramState state, Collection<ProgramState> successorStates,
                                 SemanticsCommand semanticsCommand) {
 
-        return successorStates.isEmpty() && isTerminalStatement(semanticsCommand);
+        // IMPORTANT: The return below suggests that an occurring Exception is not caught! If try/catch blocks are to be used, this might need fixing.
+        return (successorStates.isEmpty() && isTerminalStatement(semanticsCommand)) || state instanceof ExceptionProgramState;
     }
 
     private boolean isTerminalStatement(SemanticsCommand semanticsCommand) {
